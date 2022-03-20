69a16addd1316d919b20e83b24bebc1c2b668d22
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-20 02:14:12 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // plex server api for all account information
+  // http://192.168.1.243:32400/accounts/?X-Plex-Token=Gg4Av9Z2vwjJzF7mwkDt
+  
+  return (
+    <div className="App">
+      <h1>test eric</h1>
+
+    </div>
+  );
+}
+
+export default App;
